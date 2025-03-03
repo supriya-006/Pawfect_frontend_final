e8b3c0276e1ae479e61fb9ba078b546009eb30c1
UpasanaThapa <upasanathapa311@gmail.com>
2025-03-03 22:28:01 +0545

    updated

--- src/component/MyRoutes.jsx
+++ src/component/MyRoutes.jsx
@@ -1,28 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
-import Homepage from "../Homepage";
-import Layouts from "./layouts/Layouts";
-import Help from "./Help";
-import Signup from "./Signup";
-import Forget_password from "./Forget_password";
-import Reset_password from "./Reset_password";
-import Login from "./Login";
+import React from 'react'
 
 const MyRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Layouts />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/forget_password" element={<Forget_password />} />
-          <Route path="/reset_password" element={<Reset_password />} />
-          <Route path="/help" element={<Help />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-};
+    <div>MyRoutes</div>
+  )
+}
 
-export default MyRoutes;
+export default MyRoutes
