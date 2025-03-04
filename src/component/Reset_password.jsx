ef0e628fabdc66e9c21390d26d434e1b97c298ec
UpasanaThapa <upasanathapa311@gmail.com>
2025-03-04 21:58:08 +0545

    Commited

--- src/component/Reset_password.jsx
+++ src/component/Reset_password.jsx
@@ -49,8 +49,8 @@ const Reset_password = () => {
         <section className="bg-gray-50 dark:bg-gray-900">
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-              <img src="src/component/logo.jpg" alt="logo" height="80px" width="80px" />
-              Adopation
+              <img src="./logo.jpg" alt="logo" height="80px" width="80px" />
+              Adoptation
             </a>
             <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
               <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
